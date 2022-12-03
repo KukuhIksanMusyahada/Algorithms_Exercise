1faca9384861ef52802f51fe1a5b58366796bd17
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-12-03 17:21:12 +0700

    using pointer done

--- buy_sell_stock.py
+++ buy_sell_stock.py
@@ -28,6 +28,19 @@ class BuySellStock:
                     profit = new_profit
         return profit
 
+    def sol3(self,prices: list[int]):
+        l,r = 0,1
+        maxP = 0
+
+        while r<len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r +=1
+        return maxP
+
 
 if __name__=='__main__':
     prices = [7,1,5,3,6,4]
@@ -35,4 +48,6 @@ if __name__=='__main__':
     max_profit = solution.sol1(prices)
     print(max_profit)
     max_profit = solution.sol2(prices)
+    print(max_profit)
+    max_profit = solution.sol3(prices)
     print(max_profit)
